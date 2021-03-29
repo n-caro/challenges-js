474c1e346c234f9b4f2249675b5f7064b4a097e4
Nicolas <niicolas.caro@gmail.com>
2021-03-29 17:16:52 -0300

    update files to optimize tests

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,11 +9,11 @@ module.exports = {
     dialect: "mysql",
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOSTNAME,
+    port: process.env.TEST_DB_PORT,
     dialect: "mysql",
   },
   production: {
--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,9 @@ const { PORT } = require("./config");
 
 app.use("/", routes);
 
-app.listen(PORT, () => console.log(`🚀 Listening on port: ${PORT}`));
+app.listen(PORT, () => {
+  if (process.env.NODE_ENV != "test")
+    console.log(`🚀 Listening on port: ${PORT}`);
+});
+
+module.exports = app;
